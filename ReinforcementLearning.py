a33ec40c3b66f677c7a8f51d971b0df4168bc5f5
ZirvedaAytimur <zirvedaytimur@gmail.com>
2021-03-19 13:33:37 +0300

    The project has started with getting parameters.

---
+++ ReinforcementLearning.py
@@ -0,0 +1,39 @@
+import setup_path 
+import airsim
+
+import numpy as np
+import os
+import tempfile
+import pprint
+import cv2
+
+# connect to the AirSim simulator
+client = airsim.MultirotorClient()
+client.confirmConnection()
+client.enableApiControl(True)
+client.armDisarm(True)
+
+state = client.getMultirotorState()
+s = pprint.pformat(state)
+print("state: %s" % s)
+
+imu_data = client.getImuData()
+s = pprint.pformat(imu_data)
+print("imu_data: %s" % s)
+
+barometer_data = client.getBarometerData()
+s = pprint.pformat(barometer_data)
+print("barometer_data: %s" % s)
+
+magnetometer_data = client.getMagnetometerData()
+s = pprint.pformat(magnetometer_data)
+print("magnetometer_data: %s" % s)
+
+gps_data = client.getGpsData()
+s = pprint.pformat(gps_data)
+print("gps_data: %s" % s)
+
+airsim.wait_key('Press any key to reset to original state')
+
+client.armDisarm(False)
+client.reset()
